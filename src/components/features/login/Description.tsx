d500ce74487d36e45da17149bc7d85e1c8ba98d8
minji2219 <200146@jnu.ac.kr>
2024-11-21 17:08:53 +0900

    feat: 로그인 페이지 설명 컴포넌트 생성
    나를 pick하다
    너를 peep하다

---
+++ src/components/features/login/Description.tsx
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled';
+import peep from '@assets/Peep.png';
+import {COMMON} from '@styles/common';
+export const Description = () => {
+  return (
+    <Wrapper>
+      <DescriptionWrapper>
+        나를 <PickColor>Pick</PickColor>하다
+        <br /> 너를 <PeepColor>PEEP</PeepColor>하다
+      </DescriptionWrapper>
+      <img src={peep} alt="peep" />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div``;
+const DescriptionWrapper = styled.div`
+  font-family: ChangwonDangamAsacBold;
+  font-size: 4.5rem;
+`;
+
+const PickColor = styled.span`
+  color: ${COMMON.color.yellow};
+`;
+
+const PeepColor = styled.span`
+  color: ${COMMON.color.primary};
+`;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,29 @@
+import {Description} from '@components/features/login/Description';
+import {LoginBox} from '@components/features/login/LoginBox';
+import styled from '@emotion/styled';
+import {COMMON} from '@styles/common';
+
 const Login = () => {
-  return <div>로그인</div>;
+  return (
+    <Wrapper>
+      <Top>
+        <Description />
+        <LoginBox />
+      </Top>
+    </Wrapper>
+  );
 };
 
 export default Login;
+
+const Wrapper = styled.div`
+  max-width: ${COMMON.width.fullWidth};
+  margin: auto;
+  border: 1px solid red;
+`;
+
+const Top = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
