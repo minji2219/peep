153125a012e996fb56627b6a423d2722f1ac65a1
minji2219 <200146@jnu.ac.kr>
2024-11-26 00:04:07 +0900

    feat: 로그인 박스 추가 구현
    버튼 및 자동로그인 간편로그인 추가

--- src/components/features/login/LoginBox.tsx
+++ src/components/features/login/LoginBox.tsx
@@ -1,3 +1,4 @@
+import {Button} from '@components/common/Button';
 import {Input} from '@components/common/Input';
 import styled from '@emotion/styled';
 import {COMMON} from '@styles/common';
@@ -12,10 +13,28 @@ export const LoginBox = () => {
         <br />
         되셨나요?
       </Description>
+
       <Form>
         <Input placeholder="아이디" {...(register('id'), {required: true})} />
         <Input placeholder="비밀번호" {...(register('password'), {required: true})} />
+        <Label>
+          <Checkbox type="checkbox" {...register('autoLogin')} />
+          자동 로그인
+        </Label>
+        <Button onClick={() => {}}>로그인</Button>
+        <Button type="button" onClick={() => {}} bgColor={COMMON.color.darkGray}>
+          회원가입
+        </Button>
+        <Button type="button" onClick={() => {}} bgColor="transparent" style={{color: COMMON.color.lightBlack}}>
+          계정을 잊어버리셨나요?
+        </Button>
       </Form>
+
+      <SimpleLoginWrapper>
+        <Line />
+        <SimpleLogin>간편 로그인</SimpleLogin>
+        <Line />
+      </SimpleLoginWrapper>
     </Wrapper>
   );
 };
@@ -38,4 +57,37 @@ const PeepColor = styled.span`
   color: ${COMMON.color.primary};
 `;
 
-const Form = styled.form``;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const Label = styled.label`
+  color: ${COMMON.color.lightBlack};
+  cursor: pointer;
+  margin-bottom: 48px;
+`;
+
+const Checkbox = styled.input`
+  margin-right: 10px;
+  margin-left: 40px;
+`;
+
+const SimpleLoginWrapper = styled.div`
+  display: flex;
+  gap: 15px;
+  margin-top: 64px;
+`;
+
+const Line = styled.hr`
+  border: 0;
+  background-color: ${COMMON.color.lightGray};
+  width: 100%;
+  height: 1px;
+`;
+
+const SimpleLogin = styled.div`
+  color: ${COMMON.color.lightBlack};
+  white-space: nowrap;
+`;
