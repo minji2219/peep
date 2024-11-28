6ab120715752e3be0b45bc14cca29c0cbd0cff7f
minji2219 <200146@jnu.ac.kr>
2024-11-29 00:31:56 +0900

    feat: 동의, 미동의 view 분기 처리

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,11 +1,16 @@
 import {AgreementBox} from '@components/features/signup/AgreementBox';
+import {SignupBox} from '@components/features/signup/SignupBox';
+import styled from '@emotion/styled';
+import {useState} from 'react';
 
 const Signup = () => {
-  return (
-    <div>
-      <AgreementBox />
-    </div>
-  );
+  const [isAgree, setIsAgree] = useState(true);
+
+  return <Wrapper>{isAgree ? <AgreementBox setIsAgree={setIsAgree} /> : <SignupBox />}</Wrapper>;
 };
 
 export default Signup;
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
