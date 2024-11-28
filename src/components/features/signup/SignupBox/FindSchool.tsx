171fa59ee6e28be410bb6a2d9fe9e9a87039a1c2
minji2219 <200146@jnu.ac.kr>
2024-11-29 01:03:44 +0900

    feat: 학교 검색 컴포넌트 생성

---
+++ src/components/features/signup/SignupBox/FindSchool.tsx
@@ -0,0 +1,12 @@
+import {Input} from '@components/common/Input';
+import styled from '@emotion/styled';
+
+export const FindSchool = () => {
+  return (
+    <Wrapper>
+      <Input placeholder="이름검색" />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div``;
