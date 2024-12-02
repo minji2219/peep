1dcc88edcc78d2634f61545755fccdc04f7960d3
minji2219 <200146@jnu.ac.kr>
2024-12-03 02:30:41 +0900

    feat: 버튼 컴포넌트 padding값 props로 받기

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -7,22 +7,23 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   bgColor?: string;
   color?: string;
   border?: boolean;
+  padding?: string;
   onClick: () => void;
 }
-export const Button = ({children, bgColor = COMMON.color.primary, color = 'white', onClick, type = 'submit', ...rest}: Props) => {
+export const Button = ({children, padding = '12px', bgColor = COMMON.color.primary, color = 'white', onClick, type = 'submit', ...rest}: Props) => {
   return (
-    <Wrapper type={type} onClick={onClick} bgColor={bgColor} color={color} {...rest}>
+    <Wrapper type={type} padding={padding} onClick={onClick} bgColor={bgColor} color={color} {...rest}>
       {children}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.button<{border?: boolean; bgColor: string; color: string}>((props) => ({
+const Wrapper = styled.button<{padding: string; border?: boolean; bgColor: string; color: string}>((props) => ({
   backgroundColor: props.bgColor,
   fontSize: '16px',
   color: props.color,
   borderRadius: '30px',
   border: props.border ? `1px solid ${COMMON.color.lightBlack}` : 'none',
-  padding: '12px',
+  padding: props.padding,
   whiteSpace: 'nowrap',
 }));
