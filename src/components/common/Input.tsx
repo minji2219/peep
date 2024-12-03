1b9db4bef479b6dc3bd9b917a97b492d064ede39
minji2219 <200146@jnu.ac.kr>
2024-12-03 22:31:11 +0900

    feat: Input props type->theme 명칭 변경
    input의 기본 속성인 type을 사용하기 위해 명칭 변경

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -5,16 +5,16 @@ import {forwardRef, InputHTMLAttributes, Ref} from 'react';
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   placeholder: string;
   border?: boolean;
-  type?: 'normal' | 'thin';
+  theme?: 'normal' | 'thin';
 }
-export const Input = forwardRef(({placeholder, type = 'normal', ...rest}: Props, ref: Ref<HTMLInputElement>) => {
-  return <Wrapper placeholder={placeholder} type={type} {...rest} ref={ref} />;
+export const Input = forwardRef(({placeholder, theme = 'normal', ...rest}: Props, ref: Ref<HTMLInputElement>) => {
+  return <Wrapper placeholder={placeholder} theme={theme} {...rest} ref={ref} />;
 });
-const Wrapper = styled.input<{border?: boolean; type: 'normal' | 'thin'}>((props) => ({
+const Wrapper = styled.input<{border?: boolean; theme: 'normal' | 'thin'}>((props) => ({
   width: '100%',
   boxSizing: 'border-box',
-  padding: props.type === 'normal' ? '14px 40px' : '13px 24px',
+  padding: props.theme === 'normal' ? '14px 40px' : '13px 24px',
   borderRadius: '30px',
-  fontSize: props.type === 'normal' ? '20px' : '16px',
+  fontSize: props.theme === 'normal' ? '20px' : '16px',
   border: props.border ? `1px solid ${COMMON.color.lightBlack}` : 'none',
 }));
