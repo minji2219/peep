3e6d9883fe58fc15d9bf9bdf067e3ffc871d0a8d
minji2219 <200146@jnu.ac.kr>
2024-12-04 23:28:36 +0900

    feat: navigationbar 생성
    로그인, 회원가입 페이지 외의 페이지에서 navigatinbar 보이게 layout설정
    navigationbar 구현

---
+++ src/components/common/layout/NavigationBar.tsx
@@ -0,0 +1,55 @@
+import styled from '@emotion/styled';
+import {PATH} from '@routes/path';
+import {COMMON} from '@styles/common';
+import {useLocation, useNavigate} from 'react-router-dom';
+
+export const NavigationBar = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  return (
+    <Wrapper>
+      <Menu onClick={() => navigate(PATH.questions)} presentPage={location.pathname === PATH.questions}>
+        질문리스트
+      </Menu>
+      <Menu onClick={() => navigate(PATH.main)} presentPage={location.pathname === PATH.main}>
+        홈
+      </Menu>
+      <Menu onClick={() => navigate(PATH.friends)} presentPage={location.pathname === PATH.friends}>
+        친구
+      </Menu>
+      <Menu onClick={() => navigate(PATH.mypage)} presentPage={location.pathname === PATH.mypage}>
+        마이페이지
+      </Menu>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.nav`
+  display: flex;
+  gap: 100px;
+  position: absolute;
+  top: -70px;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
+const Menu = styled.div<{presentPage: boolean}>`
+  font-size: 30px;
+  cursor: pointer;
+  padding: 9px 40px;
+  white-space: nowrap;
+  ${(props) => {
+    if (props.presentPage) {
+      return `
+        color:white;
+        border:1px solid gold;
+        border-radius:30px;
+        background-color:${COMMON.color.darkGray}
+      `;
+    }
+    return `
+      color:${COMMON.color.darkGray};
+      font-weight:normal;
+    `;
+  }}
+`;
