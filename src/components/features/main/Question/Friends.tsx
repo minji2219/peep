d58762996f98222384905d7cde3eed6752e1bf59
minji2219 <200146@jnu.ac.kr>
2025-01-07 20:45:55 +0900

    feat: 픽할 친구 리스트 구현
    checkbox 형태로 구현

--- src/components/features/main/Question/Friends.tsx
+++ src/components/features/main/Question/Friends.tsx
@@ -1,19 +1,86 @@
 import styled from '@emotion/styled';
 import Friend from './Friend';
-
+import {AiOutlineUndo} from 'react-icons/ai';
+import {COMMON} from '@styles/common';
 const Friends = () => {
   const names = ['서민지', '신지훈', '박인서', '유수민'];
   return (
     <Wrapper>
-      {names.map((name) => (
-        <Friend name={name} />
-      ))}
+      <Cotainer>
+        {names.map((name) => (
+          //TODO: KEY값 추가하기
+          <Friend name={name} />
+        ))}
+      </Cotainer>
+
+      <Arrow>
+        <LeftArrow>
+          <AiOutlineUndo size="30" />
+        </LeftArrow>
+        <RightArrow>
+          <AiOutlineUndo size="30" />
+        </RightArrow>
+        <Count>2</Count>
+      </Arrow>
+
+      <PickBtn>Pick !</PickBtn>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const Cotainer = styled.div`
+  display: flex;
   justify-content: space-between;
 `;
+
+const Arrow = styled.div`
+  display: flex;
+  gap: 20px;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  padding-left: 40px;
+`;
+
+const LeftArrow = styled(Arrow)`
+  cursor: pointer;
+  transform: rotate(45deg);
+  padding: 0;
+`;
+
+const RightArrow = styled(Arrow)`
+  cursor: pointer;
+  transform: scaleX(-1) rotate(45deg);
+  padding: 0;
+`;
+
+const Count = styled.div`
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+`;
+
+const PickBtn = styled.div`
+  background-color: ${COMMON.color.darkGray};
+  width: 200px;
+  border-radius: 30px;
+  border: 1px solid gold;
+  text-align: center;
+  color: white;
+  font-weight: bold;
+  padding: 5px 0;
+  margin: 0 auto;
+  cursor: pointer;
+`;
+
 export default Friends;
