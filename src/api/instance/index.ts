fa0f43573da91fbab262b29f17106bcb8b9bfdda
minji2219 <aabb0219@naver.com>
2025-03-10 23:13:00 +0900

    feat: accessToken 만료시 refreshToken으로 새로운 토큰 값 받아오기

--- src/api/instance/index.ts
+++ src/api/instance/index.ts
@@ -1,4 +1,7 @@
+import {PATH} from '@routes/path';
 import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
+import getDeviceId from '@utils/getDeviceId';
+import {useNavigate} from 'react-router-dom';
 
 const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
   const instance = axios.create({
@@ -12,7 +15,7 @@ const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
   });
   return instance;
 };
-// const TEST_BASE_URL = `https://26fa-1-249-62-29.ngrok-free.app/`;
+// const TEST_BASE_URL = `https://a0bf-125-136-11-232.ngrok-free.app/`;
 const DEPLOY_BASE_URL = `https://port-0-peep-qxz2eltwz9onl.sel5.cloudtype.app/`;
 const BASE_URL = DEPLOY_BASE_URL;
 
@@ -36,6 +39,45 @@ fetchAuthInstance.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+fetchAuthInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+
+  async (error) => {
+    if (error?.status === 403 && error?.code === 'ERR_BAD_REQUEST') {
+      // 토큰 유효기간 만료 에러가 발생하면
+
+      try {
+        const originalRequest = error.config;
+        const refreshToken = localStorage.getItem('refreshToken');
+        const token = await fetchInstance.post('auth/refresh', '', {
+          headers: {
+            'refresh-token': `Bearer ${refreshToken}`,
+            'Device-Id': getDeviceId(),
+          },
+        });
+
+        if (token) {
+          localStorage.setItem('accessToken', token.data.accessToken);
+          localStorage.setItem('refreshToken', token.data.refreshToken);
+
+          return fetchAuthInstance.request(originalRequest);
+        }
+        // 토큰 발급이 성공적으로 되면 access token을 업데이트하고 기존 api요청을 다시 보낸다.
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (error) {
+        const navigate = useNavigate();
+        alert('세션이 만료되었습니다. 다시 로그인해 주시기 바랍니다.');
+        navigate(PATH.login);
+      }
+      // 만약 발급이 실패하였으면 refresh token도 만료가 되었음으로 로그인을 다시 해야한다.
+    }
+
     return Promise.reject(error);
   }
 );
